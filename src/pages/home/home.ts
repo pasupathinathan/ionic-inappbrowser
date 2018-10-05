e692c02169f205705197bdd50e140a350f89c852
pasupathinathan <pasupathinathan@fastura.com>
2018-10-05 10:41:31 +0530

    inapp browser

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+ // private childWindow;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private iab: InAppBrowser) {
+   // this.listenBrowser();
 
   }
+ /* listenBrowser()
+{
+window.addEventListener(“message”, (e) => {
+try {
+this.childWindow.close();
+self.close();
+} catch (error) {
+console.log(error);
+}
+}, false);
+}*/
+  ngOnInit(){
 
+    const browser = this.iab.create('https://www.javatpoint.com/','_blank',{hideurlbar:'yes',footer:'yes',closebuttoncaption:'back'});
+    
+    }
+  
 }
